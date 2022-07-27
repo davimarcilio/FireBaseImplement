bee94ee107154b82b05b34039f94ddebae30e66f
Davi Marcilio <davimarcilio.js@gmail.com>
2022-07-27 15:36:30 -0300

    login/cadastro styled

---
+++ assets/scripts/login.js
@@ -0,0 +1,14 @@
+function logar() {
+    let correctImg = document.querySelectorAll('.correct')[0];
+    correctImg.style.opacity = '1'
+    setTimeout(()=>{
+        window.location.href = '/../index.html'
+    }, 1000);
+}
+function cadastro() {
+    let correctImg = document.querySelectorAll('.correct')[1];
+    correctImg.style.opacity = '1'
+    setTimeout(()=>{
+        window.location.href = '/../index.html'
+    }, 1000);
+}
