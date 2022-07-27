7d4c7997605cea0d955d920b67642f1dc25a1408
Davi Marcilio <davimarcilio.js@gmail.com>
2022-07-27 17:17:52 -0300

    Imroviment all

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,20 +1,39 @@
 function logar() {
     let email = document.getElementById('loginemail').value;
     let password = document.getElementById('loginpassword').value;
-   
-    if (password.length > 6 && email.length > 6)  {
-        auth.createUserWithEmailAndPassword(email, password).then(()=>{
+  let buttonLogin = document.querySelectorAll('.button')[0];
+        auth.signInWithEmailAndPassword(email, password).then(()=>{
             let correctImg = document.querySelectorAll('.correct')[0];
-            correctImg.style.opacity = '1'
+          
+            correctImg.style.opacity = '1';
             setTimeout(()=>{
                 window.location.href = '/../index.html'
             }, 1000);
         }).catch((err=>{
+            //botão
+            let incorrectX = document.querySelectorAll('.incorrect')[0];
+            buttonLogin.style.backgroundColor = 'red';
+            incorrectX.style.opacity = '1'
+            // error message
+            let errorMessageHTML = document.getElementById('erro');
+            setTimeout(() => {  
+            errorMessageHTML.style.display = 'flex'
             let errCode = err.code;
             let errMsg = err.message;
+            errorMessageHTML.innerHTML = `
+            <h1 id="errorCode">${errCode}</h1>
+            <p id="errorMessage">${errMsg}</p>`;
+            }, 400);
+            setTimeout(() => {
+                let inputEmail = document.getElementById('loginemail');
+                inputEmail.focus();
+                buttonLogin.style.backgroundColor = '#282231';
+                incorrectX.style.opacity = '0';
+                errorMessageHTML.style.display = 'none';
+                errorMessageHTML.innerHTML = '';
+            }, 3000);
         }));
     }
-}
 function cadastro() {
     let correctImg = document.querySelectorAll('.correct')[1];
     correctImg.style.opacity = '1'
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,17 @@
 function load() {
     window.location.href ='assets/pages/home.html';
 }
+function logged() {
+    auth.onAuthStateChanged((user)=>{
+        if (user) {
+            // aparece a aba usuario
+            let userLogged = document.getElementById('onlylogged');
+            userLogged.style.display = 'inline';
+            // tira a opção de logar
+            let userNotLogged = document.getElementById('login');
+            userNotLogged.style.display = 'none';
+        } else {
+            
+        }
+    })
+}
