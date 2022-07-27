fc9818e15ae1b2352ea19318b944ce80b6f3aa40
Davi Marcilio <davimarcilio.js@gmail.com>
2022-07-27 18:05:47 -0300

    testes sucesso

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,21 +2,16 @@ function load(pagina) {
     console.log(pagina);
     switch (pagina) {
         case 'home':
-            window.location.href='/assets/pages/home.html';
+            window.location.href='assets/pages/home.html';
             break;
     case 'login':
-        window.location.href='/assets/pages/login.html';
+        window.location.href='/../assets/pages/login.html';
         break;
-        case 'sobre':
-            window.location.href='/assets/pages/sobre.html';
-            break;
-            // case 'contato':
-            // window.location.href='/assets/pages/sobre.html';
-            // break;
         default:
             'home';
             break;
     }
+    window.location.href ='assets/pages/home.html';
 }
 function logged() {
     auth.onAuthStateChanged((user)=>{
@@ -24,18 +19,14 @@ function logged() {
             // aparece a aba usuario
             let userLogged = document.getElementById('onlylogged');
             userLogged.style.display = 'inline';
-        } else {
-            // aparece a opção de logar
+            // tira a opção de logar
             let userNotLogged = document.getElementById('login');
-            userNotLogged.style.display = 'inline';
+            userNotLogged.style.display = 'none';
+        } else {
+            
         }
     })
-<<<<<<< Updated upstream
 }
-<<<<<<< HEAD
-=======
-}
-<<<<<<< HEAD
 // setTimeout(() => {
 //     auth.signOut().then(()=>{
 //     console.log('deslogado');
@@ -43,13 +34,3 @@ function logged() {
 //     console.log(err);
 // })
 // }, 10000);
-=======
-auth.signOut().then(()=>{
-    console.log('usuario deslogado');
-}).catch((err)=>{
-    console.log(err);
-})
->>>>>>> 17cbb44 (TEstes)
->>>>>>> Stashed changes
-=======
->>>>>>> 341d13c (Testes)
