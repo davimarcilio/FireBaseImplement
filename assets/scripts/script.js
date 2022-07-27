341d13c481f05cd52fb17d55f25ad236613252c2
Davi Marcilio <davimarcilio.js@gmail.com>
2022-07-27 17:59:27 -0300

    Testes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,16 +2,21 @@ function load(pagina) {
     console.log(pagina);
     switch (pagina) {
         case 'home':
-            window.location.href='assets/pages/home.html';
+            window.location.href='/assets/pages/home.html';
             break;
     case 'login':
-        window.location.href='/../assets/pages/login.html';
+        window.location.href='/assets/pages/login.html';
         break;
+        case 'sobre':
+            window.location.href='/assets/pages/sobre.html';
+            break;
+            // case 'contato':
+            // window.location.href='/assets/pages/sobre.html';
+            // break;
         default:
             'home';
             break;
     }
-    window.location.href ='assets/pages/home.html';
 }
 function logged() {
     auth.onAuthStateChanged((user)=>{
@@ -19,16 +24,10 @@ function logged() {
             // aparece a aba usuario
             let userLogged = document.getElementById('onlylogged');
             userLogged.style.display = 'inline';
+        } else {
             // tira a opção de logar
             let userNotLogged = document.getElementById('login');
-            userNotLogged.style.display = 'none';
-        } else {
-            
+            userNotLogged.style.display = 'inline';
         }
     })
 }
-auth.signOut().then(()=>{
-    console.log('usuario deslogado');
-}).catch((err)=>{
-    console.log(err);
-})
