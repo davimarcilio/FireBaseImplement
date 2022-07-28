1247d91a97341d5c8e82252386c28212df951030
Davi Marcilio <davimarcilio.js@gmail.com>
2022-07-28 16:03:27 -0300

    Responsivity

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -23,31 +23,76 @@ function load(page) {
 function logged() {
     let userLogged = document.getElementById('onlylogged');
     let userNotLogged = document.getElementById('login');
-    auth.onAuthStateChanged((user) => {
+    try {
+       var homeimg = document.querySelector('.homeimg');
+       var hamburgmenuopen = document.querySelector('.responsivityHamburguerMenu');
+    } catch (error) {
+        console.log(error);
+    }
+    auth.onAuthStateChanged((user) => { 
+        let meniu = document.querySelectorAll('.responsivity');
         if (user) {
             if (window.matchMedia("(min-width: 775px)").matches) {
             userNotLogged.style.display = 'none';
             userLogged.style.display = 'inline';
+            hamburgmenuopen.style.display = 'none'
+           
+            
+            try {
+                homeimg.style.position = 'relative';
+            } catch (error) {
+                console.log(error);
+            }
               } else {
+                try {
+                    homeimg.style.position = 'static';
+                } catch (error) {
+                    console.log(error);
+                }
                 userNotLogged.style.display = 'none';
                 userLogged.style.display = 'none';
+                meniu[5].style.display = 'inline';
               }
             
         } else {
+            
             if (window.matchMedia("(min-width: 775px)").matches) {
                userLogged.style.display = 'none';
             userNotLogged.style.display = 'inline';
+            
+            try {
+                homeimg.style.position = 'relative';
+            } catch (error) {
+                console.log(error);
+            }
               } else {
+                try {
+                    homeimg.style.position = 'static';
+                } catch (error) {
+                    console.log(error);
+                }
                 userLogged.style.display = 'none';
             userNotLogged.style.display = 'none';
+            meniu[4].style.display = 'inline';
               }
             
         }
     })
 }
 function ResponsiveHamMenu() {
+    var hamburgmenuopen = document.querySelector('.responsivityHamburguerMenu');
     const hamburguerMenu = document.querySelector('.hamburguer');
-    hamburguerMenu.classList.toggle('active');
+    let meniu = document.querySelectorAll('.responsivity');
+   if (hamburguerMenu.classList.toggle('active')) {
+    hamburgmenuopen.style.backgroundColor = 'aqua';
+    hamburgmenuopen.style.display = 'flex';
+    meniu[6].style.display = 'inline';
+    meniu[7].style.display = 'inline';
+   } else {
+    hamburgmenuopen.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+    hamburgmenuopen.style.display = 'none';
+   }
+    
 }
 // setTimeout(() => {
 //     auth.signOut().then(()=>{
