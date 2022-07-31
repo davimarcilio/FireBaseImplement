62423c2123520091fff8042fcbbde6b16c2c9125
Davi Marcilio <davimarcilio.js@gmail.com>
2022-07-31 13:42:18 -0300

    Start of restart project

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -28,9 +28,9 @@ function load(page) {
         case 'produtos':
             window.location.href = `${pagename}/assets/pages/produtos.html`;
             break;
-            case 'carrinho':
-                window.location.href = `${pagename}/assets/pages/carrinho.html`;
-                break;
+        case 'carrinho':
+            window.location.href = `${pagename}/assets/pages/carrinho.html`;
+            break;
         default:
             window.location.href = `${pagename}/assets/pages/home.html`;
             break;
@@ -64,12 +64,12 @@ function TrySobreHome(argumentnum) {
         }
         try {
             userImg.style.display = 'flex';
-            
+
         } catch (error) {
             console.log(error);
         }
         //Pagina Home
-    } if(argumentnum == '2') {
+    } if (argumentnum == '2') {
         try {
             sobre.style.position = 'static';
         } catch (error) {
@@ -81,126 +81,126 @@ function TrySobreHome(argumentnum) {
         } catch (error) {
             console.log(error);
         }
-       
+
     }
 }
-function logged() {
-    let userLogged = document.getElementById('onlylogged');
-    var hamburgmenuopen = document.querySelector('.responsivityHamburguerMenu');
-    let userNotLogged = document.getElementById('login');
-    auth.onAuthStateChanged((user) => {
-        let admin = document.getElementById('admin');
-        let meniu = document.querySelectorAll('.responsivity');
-        const hamburguerMenu = document.querySelector('.hamburguer');
-        let responsivityHamburguerMenu = document.querySelector('.responsivityHamburguerMenu');
-        if (user) {
-            try {
-                if (user.uid == 'g3FXmjoYUbgyeNg2y0l7x00PPsv2') {
+// function logged() {
+//     let userLogged = document.getElementById('onlylogged');
+//     var hamburgmenuopen = document.querySelector('.responsivityHamburguerMenu');
+//     let userNotLogged = document.getElementById('login');
+//     auth.onAuthStateChanged((user) => {
+//         let admin = document.getElementById('admin');
+//         let meniu = document.querySelectorAll('.responsivity');
+//         const hamburguerMenu = document.querySelector('.hamburguer');
+//         let responsivityHamburguerMenu = document.querySelector('.responsivityHamburguerMenu');
+//         if (user) {
+//             try {
+//                 if (user.uid == 'g3FXmjoYUbgyeNg2y0l7x00PPsv2') {
 
-                    meniu[8].style.display = 'inline';
-                    if (window.matchMedia("(min-width: 775px)").matches) {
-                        userNotLogged.style.display = 'none';
-                        userLogged.style.display = 'inline';
-                        hamburgmenuopen.style.display = 'none';
-                        admin.style.display = 'inline';
-                        meniu[4].style.display = 'none';
-                        meniu[5].style.display = 'none';
-                        meniu[6].style.display = 'none';
-                        meniu[7].style.display = 'none';
-                        meniu[8].style.display = 'none';
-                        responsivityHamburguerMenu.style.display = 'none';
-                        TrySobreHome('1');
-                    } else {
-                        TrySobreHome('2');
-                        responsivityHamburguerMenu.style.display = 'flex';
-                        if (hamburguerMenu.classList.contains("active")) {
-                            meniu[4].style.display = 'none';
-                            meniu[5].style.display = 'inline';
-                            meniu[6].style.display = 'inline';
-                            meniu[7].style.display = 'inline';
-                            meniu[8].style.display = 'inline';
-                        } else {
-                            meniu[4].style.display = 'none';
-                            meniu[5].style.display = 'none';
-                            meniu[6].style.display = 'none';
-                            meniu[7].style.display = 'none';
-                            meniu[8].style.display = 'none';
-                        }
-                        admin.style.display = 'none';
-                        userNotLogged.style.display = 'none';
-                        userLogged.style.display = 'none';
+//                     meniu[8].style.display = 'inline';
+//                     if (window.matchMedia("(min-width: 775px)").matches) {
+//                         userNotLogged.style.display = 'none';
+//                         userLogged.style.display = 'inline';
+//                         hamburgmenuopen.style.display = 'none';
+//                         admin.style.display = 'inline';
+//                         meniu[4].style.display = 'none';
+//                         meniu[5].style.display = 'none';
+//                         meniu[6].style.display = 'none';
+//                         meniu[7].style.display = 'none';
+//                         meniu[8].style.display = 'none';
+//                         responsivityHamburguerMenu.style.display = 'none';
+//                         TrySobreHome('1');
+//                     } else {
+//                         TrySobreHome('2');
+//                         responsivityHamburguerMenu.style.display = 'flex';
+//                         if (hamburguerMenu.classList.contains("active")) {
+//                             meniu[4].style.display = 'none';
+//                             meniu[5].style.display = 'inline';
+//                             meniu[6].style.display = 'inline';
+//                             meniu[7].style.display = 'inline';
+//                             meniu[8].style.display = 'inline';
+//                         } else {
+//                             meniu[4].style.display = 'none';
+//                             meniu[5].style.display = 'none';
+//                             meniu[6].style.display = 'none';
+//                             meniu[7].style.display = 'none';
+//                             meniu[8].style.display = 'none';
+//                         }
+//                         admin.style.display = 'none';
+//                         userNotLogged.style.display = 'none';
+//                         userLogged.style.display = 'none';
 
-                    }
-                } else {
-                    if (window.matchMedia("(min-width: 775px)").matches) {
-                        userNotLogged.style.display = 'none';
-                        admin.style.display = 'none';
-                        userLogged.style.display = 'inline';
-                        hamburgmenuopen.style.display = 'none'
-                        meniu[4].style.display = 'none';
-                        meniu[5].style.display = 'none';
-                        meniu[6].style.display = 'none';
-                        meniu[7].style.display = 'none';
-                        meniu[8].style.display = 'none';
-                        responsivityHamburguerMenu.style.display = 'none';
-                        TrySobreHome('1');
-                    } else {
-                        TrySobreHome('2');
-                        responsivityHamburguerMenu.style.display = 'flex';
-                        if (hamburguerMenu.classList.contains("active")) {
-                            meniu[4].style.display = 'none';
-                            meniu[5].style.display = 'inline';
-                            meniu[6].style.display = 'inline';
-                            meniu[7].style.display = 'inline';
-                            meniu[8].style.display = 'none';
-                        } else {
-                            meniu[4].style.display = 'none';
-                            meniu[5].style.display = 'none';
-                            meniu[6].style.display = 'none';
-                            meniu[7].style.display = 'none';
-                            meniu[8].style.display = 'none';
-                        }
-                        admin.style.display = 'none';
-                        userNotLogged.style.display = 'none';
-                        userLogged.style.display = 'none';
+//                     }
+//                 } else {
+//                     if (window.matchMedia("(min-width: 775px)").matches) {
+//                         userNotLogged.style.display = 'none';
+//                         admin.style.display = 'none';
+//                         userLogged.style.display = 'inline';
+//                         hamburgmenuopen.style.display = 'none'
+//                         meniu[4].style.display = 'none';
+//                         meniu[5].style.display = 'none';
+//                         meniu[6].style.display = 'none';
+//                         meniu[7].style.display = 'none';
+//                         meniu[8].style.display = 'none';
+//                         responsivityHamburguerMenu.style.display = 'none';
+//                         TrySobreHome('1');
+//                     } else {
+//                         TrySobreHome('2');
+//                         responsivityHamburguerMenu.style.display = 'flex';
+//                         if (hamburguerMenu.classList.contains("active")) {
+//                             meniu[4].style.display = 'none';
+//                             meniu[5].style.display = 'inline';
+//                             meniu[6].style.display = 'inline';
+//                             meniu[7].style.display = 'inline';
+//                             meniu[8].style.display = 'none';
+//                         } else {
+//                             meniu[4].style.display = 'none';
+//                             meniu[5].style.display = 'none';
+//                             meniu[6].style.display = 'none';
+//                             meniu[7].style.display = 'none';
+//                             meniu[8].style.display = 'none';
+//                         }
+//                         admin.style.display = 'none';
+//                         userNotLogged.style.display = 'none';
+//                         userLogged.style.display = 'none';
 
-                    }
-                }
-            } catch (error) {
-                console.log(error);
-            }
+//                     }
+//                 }
+//             } catch (error) {
+//                 console.log(error);
+//             }
 
-        } else {
+//         } else {
 
-            if (window.matchMedia("(min-width: 775px)").matches) {
-                userLogged.style.display = 'none';
-                userNotLogged.style.display = 'inline';
-                admin.style.display = 'none';
-                meniu[4].style.display = 'none';
-                meniu[5].style.display = 'none';
-                meniu[6].style.display = 'none';
-                meniu[7].style.display = 'none';
-                meniu[8].style.display = 'none';
-                responsivityHamburguerMenu.style.display = 'none';
-                TrySobreHome('1');
-            } else {
-                TrySobreHome('2');
-                // ResponsiveHamMenu();
-                if (hamburguerMenu.classList.contains("active")) {
-                    meniu[4].style.display = 'inline';
-                    meniu[5].style.display = 'none';
-                    meniu[6].style.display = 'inline';
-                    meniu[7].style.display = 'inline';
-                    meniu[8].style.display = 'inline';
-                }
-                admin.style.display = 'none';
-                userLogged.style.display = 'none';
-                userNotLogged.style.display = 'none';
-            }
+//             if (window.matchMedia("(min-width: 775px)").matches) {
+//                 userLogged.style.display = 'none';
+//                 userNotLogged.style.display = 'inline';
+//                 admin.style.display = 'none';
+//                 meniu[4].style.display = 'none';
+//                 meniu[5].style.display = 'none';
+//                 meniu[6].style.display = 'none';
+//                 meniu[7].style.display = 'none';
+//                 meniu[8].style.display = 'none';
+//                 responsivityHamburguerMenu.style.display = 'none';
+//                 TrySobreHome('1');
+//             } else {
+//                 TrySobreHome('2');
+//                 // ResponsiveHamMenu();
+//                 if (hamburguerMenu.classList.contains("active")) {
+//                     meniu[4].style.display = 'inline';
+//                     meniu[5].style.display = 'none';
+//                     meniu[6].style.display = 'inline';
+//                     meniu[7].style.display = 'inline';
+//                     meniu[8].style.display = 'inline';
+//                 }
+//                 admin.style.display = 'none';
+//                 userLogged.style.display = 'none';
+//                 userNotLogged.style.display = 'none';
+//             }
 
-        }
-    })
-}
+//         }
+//     })
+// }
 function ResponsiveHamMenu() {
     var hamburgmenuopen = document.querySelector('.responsivityHamburguerMenu');
     const hamburguerMenu = document.querySelector('.hamburguer');
@@ -215,5 +215,17 @@ function ResponsiveHamMenu() {
         hamburgmenuopen.style.backgroundColor = 'rgba(255, 255, 255, 0)';
         hamburgmenuopen.style.display = 'none';
     }
-
+}
+function UserOrNot() {
+    let UserOrNotDiv = document.getElementById('UserOrNot');
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+            UserOrNotDiv.innerHTML = `<a id="onlylogged" class="responsivity" onclick="load('user')"><li>User</li></a>`;
+            if (user.uid == 'g3FXmjoYUbgyeNg2y0l7x00PPsv2') {
+               UserOrNotDiv.innerHTML += `<a id="admin" class="responsivity" onclick="load('admin')"><li>Admin</li></a>`
+            }
+        }else{
+           UserOrNotDiv.innerHTML = `<a id="login" class="responsivity" onclick="load('login')"><li>Entrar</li></a>`
+        }
+    })
 }
